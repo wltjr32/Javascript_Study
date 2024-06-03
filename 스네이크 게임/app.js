fd8fa2fb751215d04c9912a769baaecf92d73993
wltjr32 <fcjs9@naver.com>
2024-06-03 16:29:28 +0900

    snake1

---
+++ 스네이크 게임/app.js
@@ -0,0 +1,23 @@
+for(let i=0; i<100; i++) {
+    const square = document.createElement("div");
+    document.querySelector(".grid").appendChild(square);
+}
+const grid = document.querySelector(".grid");
+const squares = document.querySelector(".grid div");
+const startBtn = document.querySelector("#startBtn");
+const stopBtn = document.querySelector("#stopBtn");
+const scoreDisplay = document.querySelector("#score");
+const width = 10;
+let snake = 0;
+
+function start() {
+    squares[snake].classList.add("snake");
+    runGame();
+    runGame();
+}
+function runGame() {
+    snake = snake + 1;
+    squares[snake].classList.add("snake");
+}
+
+startBtn.addEventListener("click", start);
