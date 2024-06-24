e5018ac638c4b910fe05ee78350ebfaa92395284
wltjr32 <fcjs9@naver.com>
2024-06-24 14:13:52 +0900

    스페이스 인베이더

--- 스페이스 인베이더/app.js
+++ 스페이스 인베이더/app.js
@@ -11,3 +11,43 @@ const stage = document.querySelectorAll(".grid div");
 const startBtn = document.querySelector("#startBtn");
 const stopBtn = document.querySelector("#stopBtn");
 const display = document.querySelector("#display");
+
+let playerLoc = stageSize*(stageSize-2)+7 // 202;
+
+function makePlayer() {
+    stage[playerLoc].classList.add("player");
+}
+makePlayer()
+
+function movePlayer(e) {
+    stage[playerLoc].classList.remove("player");
+    switch (e.keyCode) {
+        case 37: //왼쪽
+            if (playerLoc % stageSize !==0) { playerLoc--; }
+            break;
+        case 39: //오른쪽
+            if (playerLoc % stageSize < stageSize-1) { playerLoc++; }
+            break;
+    }
+    stage[playerLoc].classList.add("player")
+}
+
+document.addEventListener("keyup", movePlayer)
+
+let invaderLoc = [
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+    30, 31, 32, 33, 34, 35, 36, 37, 38, 39
+]
+let gameInterval;
+
+function makeInvader() {
+    invaderLoc.forEach(function(invader) {
+        stage[invader].classList.add("invader");
+    })
+}
+makeInvader();
+
+function moveInvader() {}
+
+interval = setInterval(moveInvader, 1000);
