f2e80691d5c068bc7c6a078588882a7bdac4608a
wltjr32 <fcjs9@naver.com>
2024-06-24 13:16:31 +0900

    스페이스인베이더 게임

---
+++ 스페이스 인베이더/app.js
@@ -0,0 +1,13 @@
+const stageSize = 15;
+
+//스테이지 생성
+for(let i=0; i < stageSize*stageSize; i++) {
+    const box = document.createElement("div");
+    document.querySelector(".grid").appendChild(box);
+}
+
+const grid = document.querySelector(".grid");
+const stage = document.querySelectorAll(".grid div");
+const startBtn = document.querySelector("#startBtn");
+const stopBtn = document.querySelector("#stopBtn");
+const display = document.querySelector("#display");
